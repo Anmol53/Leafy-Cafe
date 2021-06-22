9a5a9c8075d592818226c1f9f42a6ed98349edd6
Anmol Agrawal <anmol.ag53@gmail.com>
2021-06-23 02:45:33 +0530

    Adding Data from json instead of static

--- js/index.js
+++ js/index.js
@@ -55,16 +55,48 @@ const heroBG = (numberOfBGs) => {
         source.type = 'video/mp4';
         video.appendChild(source);
         video.load();
-        video.oncanplaythrough = function () {
+        video.oncanplaythrough = function() {
             bg.appendChild(video);
         };
     }
 }
 
+const loadMenu = () => {
+    fetch('../data/menu.json')
+        .then(response => response.json())
+        .then(menu => {
+            menu.forEach(category => {
+                const {
+                    title,
+                    abbreviation: abbr,
+                    emoji,
+                    items
+                } = category;
+                const menuContent = document.querySelector(`.${abbr}-section > .menu-container > .menu-content`);
+                const contentHeading = document.createElement('h1');
+                contentHeading.innerHTML = `${title} <span>${emoji}</span>`;
+                menuContent.appendChild(contentHeading);
+                const list = document.createElement('ul');
+                items.forEach(item => {
+                    const listItem = document.createElement('li');
+                    const itemTitle = document.createElement('h2');
+                    itemTitle.innerText = item.title;
+                    const itemDescription = document.createElement('p');
+                    itemDescription.innerText = item.description;
+                    listItem.appendChild(itemTitle);
+                    listItem.appendChild(itemDescription);
+                    list.appendChild(listItem);
+                });
+                menuContent.appendChild(list);
+            });
+        })
+        .catch(err => console.log(err));
+}
 
 navSlide();
 slideText();
 heroBG(4);
+loadMenu();
 
 // (init() => {
 // })();
